54cacf2ad37bbf3860e826e9dc73d82b815f0962
vitalii2016 <vitalii2016@users.noreply.github.com>
2016-03-25 23:45:00 +0300

    Added files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# coding=UTF-8
+
+
+import scipy.stats
+
+
+def stat(sample, n):
+    for i in xrange(n):
+        moment = [0 for _ in xrange(2 ** (i + 1))]
+        for j in xrange(len(sample) - i):
+            index = int(''.join(map(str, sample[j:(j + i + 1)])), 2)
+            moment[index] += 1
+
+        # Напечатать распределения (чтобы работало, в следующей строке удалить '#')
+        # print moment
+
+        # Напечатать p-значения
+        print scipy.stats.chisquare(moment)[1]
+
+
+def main():
+    file = open('sample.txt', 'r')
+    lines = file.readlines()
+    file.close()
+    sample = map(int, lines[0])
+
+    # Второй аргумент --- максимальный размер (т.е. n) подсчитываемых n-грамм
+    stat(sample, 10)
+
+
+if __name__ == '__main__':
+    main()
